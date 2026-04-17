ad2cba8ca81a2952cf8a1b1c64b53d38647b49fd
robert.costa <robert.costa@cvm.gov.br>
2026-04-17 15:57:54 -0300

    CORS corrigindo 2

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nology.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
